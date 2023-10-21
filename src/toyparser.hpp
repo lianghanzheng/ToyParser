b9b93a75fc0a6bda694f06386b87a8eb6132acb3
lianghanzheng <915625352@qq.com>
2023-10-21 18:06:29 +0800

    lesson1 complete

--- src/toyparser.hpp
+++ src/toyparser.hpp
@@ -1,23 +1,106 @@
 #include <iostream>
 #include <string>
+#include "boost/json/src.hpp"
 
 using namespace std;
+namespace json = boost::json;
+
+void
+pretty_print( std::ostream& os, json::value const& jv, std::string* indent = nullptr )
+{
+    std::string indent_;
+    if(! indent)
+        indent = &indent_;
+    switch(jv.kind())
+    {
+    case json::kind::object:
+    {
+        os << "{\n";
+        indent->append(4, ' ');
+        auto const& obj = jv.get_object();
+        if(! obj.empty())
+        {
+            auto it = obj.begin();
+            for(;;)
+            {
+                os << *indent << json::serialize(it->key()) << " : ";
+                pretty_print(os, it->value(), indent);
+                if(++it == obj.end())
+                    break;
+                os << ",\n";
+            }
+        }
+        os << "\n";
+        indent->resize(indent->size() - 4);
+        os << *indent << "}";
+        break;
+    }
+
+    case json::kind::array:
+    {
+        os << "[\n";
+        indent->append(4, ' ');
+        auto const& arr = jv.get_array();
+        if(! arr.empty())
+        {
+            auto it = arr.begin();
+            for(;;)
+            {
+                os << *indent;
+                pretty_print( os, *it, indent);
+                if(++it == arr.end())
+                    break;
+                os << ",\n";
+            }
+        }
+        os << "\n";
+        indent->resize(indent->size() - 4);
+        os << *indent << "]";
+        break;
+    }
+
+    case json::kind::string:
+    {
+        os << json::serialize(jv.get_string());
+        break;
+    }
+
+    case json::kind::uint64:
+    case json::kind::int64:
+    case json::kind::double_:
+        os << jv;
+        break;
+
+    case json::kind::bool_:
+        if(jv.get_bool())
+            os << "true";
+        else
+            os << "false";
+        break;
+
+    case json::kind::null:
+        os << "null";
+        break;
+    }
+
+    if(indent->empty())
+        os << "\n";
+}
 
 class ASTNode {
 public:
-  ASTNode(string type_name): type_name(type_name) {};
+  ASTNode(string type_name)
+    : info() { this->info["type_name"] = type_name; }
+  boost::json::object getInfo() { return this->info; }
 
 protected:
-  string type_name;
+  boost::json::object info;
 };
 
 class NumLiteralNode: public ASTNode {
 public:
   NumLiteralNode(string value)
-    : ASTNode("NumLiteral"),
-      value(value) {};
-private:
-  string value;  
+    : ASTNode("NumLiteral") { this->info["value"] = value; }
 };
 
 /**
